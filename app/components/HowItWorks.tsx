51a56f72fdd565948176478b8a68c802846ee186
ThiagoC0STA <thiago@lacostacorretora.com.br>
2025-06-05 18:13:51 -0300

    build

--- app/components/HowItWorks.tsx
+++ app/components/HowItWorks.tsx
@@ -1,45 +1,7 @@
 "use client";
 
-import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-import {
-  ClipboardDocumentCheckIcon,
-  DocumentTextIcon,
-  ClockIcon,
-  SparklesIcon,
-} from "@heroicons/react/24/outline";
-import Image from "next/image";
-import { useRef } from "react";
-import { useScroll, useTransform } from "framer-motion";
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
-
-const steps = [
-  {
-    icon: ClipboardDocumentCheckIcon,
-    title: "Escolha seu plano",
-    description:
-      "Selecione o tipo de consórcio e o valor que melhor se adequa ao seu perfil.",
-  },
-  {
-    icon: DocumentTextIcon,
-    title: "Faça sua proposta",
-    description:
-      "Preencha o formulário com seus dados e escolha o prazo de pagamento.",
-  },
-  {
-    icon: ClockIcon,
-    title: "Aguarde a contemplação",
-    description:
-      "Acompanhe seu processo através do seu email ou com nossos corretores",
-  },
-  {
-    icon: SparklesIcon,
-    title: "Realize seu sonho",
-    description:
-      "Após contemplado, receba seu bem e comece a usufruir imediatamente.",
-  },
-];
 
 const HowItWorksDesktop = dynamic(() => import("./HowItWorksDesktop"), {
   ssr: false,
